67f0c3cb41d2a7d2099b39f13965025e47e918f2
WaqarAliBhatti <waqar.hassan026@gmail.com>
2025-12-19 19:59:23 +0500

    WAB-- Added Over Map and Undo Button

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ ________________________________________________________________________________
 | Hassaan   | Retain Focused Button Until Scorecard is Updated |      N/A (In Dev)    |  16-12-25
 | Waqar     | Removed Unnecessary Codewith simple logic        |      N/A (In Dev)    |  19-12-25    
 | -----     | After adding to score board inactive button      |         ---          |     -    
+| Waqar     | Over Map and Undo Button                         |      N/A (In Dev)    |  19-12-25    
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Getting the score from the InputScore element
 const score = document.getElementById("InputScore").innerText;
-
-// Inserting the score into the "score" div
 document.getElementById("score").innerHTML = `${score}`;
 const inputScore = document.getElementById("InputScore");
 const scoreDiv = document.getElementById("score");
 const widebtn = document.getElementById("widebtn");
 const noBallbtn = document.getElementById("noBallbtn");
+const overMapperDiv = document.getElementById("currentOverRunsDisplay");
 
+let fairBall = 0;
+let undoStack = [];
 // Variables will be used for adding Extras
 let isNoball = false;
 let isWideball = false;
@@ -39,23 +40,68 @@ inputScore.addEventListener("keypress", function (event) {
     if (Number(inputScore.value) < 7) {
       const value = Number(inputScore.value); // get typed score
       let extraScore = 0;
+      let extraScoreText = "";
+
+      if (!isWideball && !isNoball) {
+        fairBall += 1;
+      }
 
       if (isWideball) {
         extraScore = 1;
         widebtn.classList.remove("active");
+        extraScoreText = "(WD)";
         isWideball = false; // set default value
       }
 
       if (isNoball) {
         extraScore = 1;
         noBallbtn.classList.remove("active");
+        extraScoreText = "(NB)";
         isNoball = false; // set default value
       }
 
       scoreDiv.innerHTML = Number(scoreDiv.innerHTML) + Number(value) + Number(extraScore); // display it in #score after adding the previous score
+      undoStack.push(Number(value) + Number(extraScore)); // pushing the new score to undo stack
+      currentBallRuns = `${Number(value)}${extraScoreText}`; // pushing the new score to current over runs
       inputScore.value = ""; // clear input after enter
+      const isOverFinished = fairBall === 6;
+
+      renderButtons(isOverFinished, currentBallRuns); // render buttons for current over
     } else {
       alert(`Can not add ${Number(inputScore.value)}`)
     }
   }
 });
+
+// Function to undo last added score
+function undoLastScore() {
+  const lastScoreAdded = undoStack.pop(); // removing last score from stack
+
+  if (lastScoreAdded !== undefined) {
+    const currentScore = Number(scoreDiv.innerHTML);
+
+    const newScore = currentScore - lastScoreAdded;
+    scoreDiv.innerHTML = newScore;
+    overMapperDiv.removeChild(overMapperDiv.lastChild); // removing last button from current over display
+    fairBall -= 1; // reducing fair ball count by 1
+  }
+}
+
+// Whenever score updates, this function will be called to render buttons 
+function renderButtons(isOverFinished, value) {
+  // Clear previous content
+  if (isOverFinished) {
+    setTimeout(() => {
+      overMapperDiv.innerHTML = "";
+    }, 2000);
+    return;
+  }
+
+  let button = document.createElement("button");
+  button.classList.add("over-run-map-button");
+  let span = document.createElement("span");
+  span.textContent = value; // put the value inside span
+  button.appendChild(span); // put span inside button
+  overMapperDiv.appendChild(button); // add button to the div
+
+}
